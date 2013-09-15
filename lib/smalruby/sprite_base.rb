d19d9e4fbd1b0ac0d61ddc3130554fb824a6aeb6
Kouji Takao <kouji.takao@gmail.com>
2013-09-15 23:11:49 +0900

    added SpriteBase#turn.

--- lib/smalruby/sprite_base.rb
+++ lib/smalruby/sprite_base.rb
@@ -6,6 +6,8 @@ module Smalruby
   class SpriteBase < Base
     def initialize(x, y, path)
       @sprite = Sprite.new(x, y, Image.load(path))
+      @sprite.scale_x = 1.0
+      @sprite.scale_y = 1.0
       @vector = { x: 1, y: 0 }
       super()
     end
@@ -17,16 +19,21 @@ module Smalruby
       self.y += @vector[:y] * val
     end
 
+    def turn
+      @vector[:x] *= -1
+      @vector[:y] *= -1
+      @sprite.scale_x *= -1
+      #@sprite.scale_y *= -1
+    end
+
     def turn_if_reach_wall
       max_width = Window.width - @sprite.image.width
       if x < 0
         self.x = 0
-        @vector[:x] = 1
-        @vector[:y] = 0
+        turn
       elsif x >= max_width
         self.x = max_width - 1
-        @vector[:x] = -1
-        @vector[:y] = 0
+        turn
       end
     end
 
--- samples/car.rb
+++ samples/car.rb
@@ -2,27 +2,8 @@ require 'smalruby'
 
 car1 = Car.new
 car1.on(:start) do
-  speed = 10 + rand(20)
   loop do
-    move(speed)
-    turn_if_reach_wall
-  end
-end
-
-car2 = Car.new(0, 200)
-car2.on(:start) do
-  speed = 2 + rand(5)
-  loop do
-    move(speed)
-    turn_if_reach_wall
-  end
-end
-
-car3 = Car.new(0, 350)
-car3.on(:start) do
-  speed = 1 + rand(10)
-  loop do
-    move(speed)
+    move(10)
     turn_if_reach_wall
   end
 end

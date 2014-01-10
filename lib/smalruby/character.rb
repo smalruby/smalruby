86589bf71747a2d48663a065b63320250ba31975
Kouji Takao <kouji.takao@gmail.com>
2014-01-10 15:30:43 +0900

    coding styleの修正

--- lib/smalruby/character.rb
+++ lib/smalruby/character.rb
@@ -233,7 +233,8 @@ module Smalruby
     private
 
     def asset_path(name)
-      paths = [Pathname("../#{name}").expand_path(Pathname($PROGRAM_NAME).expand_path(Dir.pwd)),
+      program_path = Pathname($PROGRAM_NAME).expand_path(Dir.pwd)
+      paths = [Pathname("../#{name}").expand_path(program_path),
                Pathname("../../../assets/#{name}").expand_path(__FILE__)]
       paths.find { |path| path.file? }.to_s
     end

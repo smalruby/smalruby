bf97c60b333add8757ae0fd5c4e5decc7af012d5
Kouji Takao <kouji.takao@gmail.com>
2014-05-08 03:52:39 +0900

    車のサンプルに回転を加えた

--- samples/car.rb
+++ samples/car.rb
@@ -2,14 +2,27 @@
 require 'smalruby'
 
 car1 = Character.new(x: 0, y: 0, costume: 'car1.png')
+car1.rotation_style = :free
 
 car1.on(:start) do
   loop do
-    move(5)
+    move(15)
     turn_if_reach_wall
   end
 end
 
 car1.on(:click) do
-  rotate(45)
+  rotate(15)
+end
+
+car1.on(:key_push, K_1) do
+  self.rotation_style = :free
+end
+
+car1.on(:key_push, K_2) do
+  self.rotation_style = :left_right
+end
+
+car1.on(:key_push, K_3) do
+  self.rotation_style = :none
 end

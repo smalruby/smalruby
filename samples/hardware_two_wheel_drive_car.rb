2cb3c1894d36d116fd0592837128812a7b647ead
Kouji Takao <kouji.takao@gmail.com>
2014-06-07 22:53:54 +0900

    ピンをD6に修正

--- samples/hardware_two_wheel_drive_car.rb
+++ samples/hardware_two_wheel_drive_car.rb
@@ -17,15 +17,15 @@ stage1.on(:start) do
   draw_font(string: DESCRIPTION, color: 'black')
 
   loop do
-    two_wheel_drive_car('D2').forward
+    two_wheel_drive_car('D6').forward
     sleep(3)
-    two_wheel_drive_car('D2').backward
+    two_wheel_drive_car('D6').backward
     sleep(3)
-    two_wheel_drive_car('D2').turn_left
+    two_wheel_drive_car('D6').turn_left
     sleep(3)
-    two_wheel_drive_car('D2').turn_right
+    two_wheel_drive_car('D6').turn_right
     sleep(3)
-    two_wheel_drive_car('D2').stop
+    two_wheel_drive_car('D6').stop
     sleep(3)
   end
 end
---
+++ samples/hardware_two_wheel_drive_car_run.rb
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+DESCRIPTION = <<EOS
+2WD車のタイヤ(モーター)を操作します
+EOS
+
+# デジタルの2番・3番ピンに左のモーター、4番・5番ピンに右のモーターを接
+# 続してください。
+
+require 'smalruby'
+
+init_hardware
+
+stage1 = Stage.new(color: 'white')
+
+stage1.on(:start) do
+  draw_font(string: DESCRIPTION, color: 'black')
+
+  loop do
+    two_wheel_drive_car('D6').run(command: :forward, sec: 2, left_level: 85, right_level: 100)
+    sleep(1)
+    two_wheel_drive_car('D6').run(command: :backward, sec: 2, left_level: 85, right_level: 100)
+    sleep(1)
+    two_wheel_drive_car('D6').run(command: :turn_left, sec: 2, left_level: 85, right_level: 100)
+    sleep(1)
+    two_wheel_drive_car('D6').run(command: :turn_right, sec: 2, left_level: 85, right_level: 100)
+    sleep(1)
+  end
+end

6972dbbcab766c78cc67dfcee92b17aabe3f885c
Kouji Takao <kouji.takao@gmail.com>
2014-08-08 01:05:51 +0900

    Windowsでのハードウェアの入力の取りこぼしを防ぐように修正

--- lib/smalruby/hardware/dino/fix_gets.rb
+++ lib/smalruby/hardware/dino/fix_gets.rb
@@ -7,16 +7,43 @@ if Util.windows?
   require 'dino'
 
   if Dino::VERSION >= '0.11'
+    require 'mutex_m'
+
     class Dino::TxRx::Base
-      def gets(timeout = 0.005)
-        return nil unless IO.select([io], nil, nil, timeout)
-        io.read_timeout = (timeout * 1000).to_i
-        bytes = []
-        until (x = io.getbyte).nil?
-          bytes.push(x)
+      def write(message)
+        loop do
+          if IO.select(nil, [io], nil)
+            io.synchronize { io.syswrite(message) }
+            break
+          end
         end
-        return nil if bytes.empty?
-        bytes.pack("C*")
+      end
+
+      def gets(timeout = 0.005)
+        @buffer ||= ""
+        if !@buffer.include?("\n")
+          return nil unless IO.select([io], nil, nil, timeout)
+          io.synchronize do
+            io.read_timeout = (timeout * 1000).to_i
+          end
+          bytes = []
+          until (x = io.synchronize { io.getbyte }).nil?
+            bytes.push(x)
+          end
+          return nil if bytes.empty?
+          @buffer += bytes.pack("C*")
+        end
+        @buffer.slice!(/\A[^\n]*\n/)
+      end
+    end
+
+    class Dino::TxRx::Serial
+      def io
+        unless @io
+          @io = connect
+          @io.extend(Mutex_m)
+        end
+        @io
       end
     end
   elsif Dino::VERSION >= '0.10'

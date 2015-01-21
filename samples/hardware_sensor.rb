c379d37e3221eb154d74adc0792729b4c704749a
Kouji Takao <kouji.takao@gmail.com>
2015-01-21 22:40:08 +0900

    [#75770724] fixed.

--- samples/hardware_sensor.rb
+++ samples/hardware_sensor.rb
@@ -6,19 +6,21 @@ init_hardware
 
 stage1 = Stage.new(color: 'white')
 
-stage1.on(:sensor_change, 'A0') do |value|
-  v = sensor('A0').value
-  if 0 <= v && v <= 255
-    fill(color: 'red')
-  end
+stage1.on(:start) do
+  loop do
+    v = sensor('A0').value
+    if 0 <= v && v <= 255
+      fill(color: 'red')
+    end
 
-  if 256 <= v && v <= 511
-    fill(color: 'yellow')
-  end
+    if 256 <= v && v <= 511
+      fill(color: 'yellow')
+    end
 
-  if 512 <= v
-    fill(color: 'blue')
-  end
+    if 512 <= v
+      fill(color: 'blue')
+    end
 
-  draw_font(string: "センサーの情報：#{v}",  color: 'black')
+    draw_font(string: "センサーの情報：#{v}",  color: 'black')
+  end
 end

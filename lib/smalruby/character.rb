82ab6d4f510d72f1ba39d0f4168c0af1192d5610
Yuki Ueda <ikuyadeu@yahoo.co.jp>
2016-03-08 15:48:04 +0900

    add ","

--- lib/smalruby/character.rb
+++ lib/smalruby/character.rb
@@ -410,7 +410,7 @@ module Smalruby
     # @param Integer val shade
     def pen_shade=(val)
       val %= 101
-      h, s = *Color.rgb_to_hsl(*pen_color)
+      h, s, = *Color.rgb_to_hsl(*pen_color)
       @pen_color = Color.hsl_to_rgb(h, s, val)
     end
 

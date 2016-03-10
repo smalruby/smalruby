cb23fd75ad4f8a6406eff05b2665afdd20d9dc53
Kouji Takao <kouji.takao@gmail.com>
2016-03-10 13:31:29 +0900

    suppress warning for spec.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,7 +12,6 @@ Dir.glob(File.expand_path('../support/**/*.rb', __FILE__)).each do |path|
 end
 
 RSpec.configure do |config|
-  config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
   config.filter_run :focus
 

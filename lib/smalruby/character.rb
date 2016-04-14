187445f9d6a71d945faba02554fa6a06301209f1
Nobuyuki Honda <nobyuki@gmail.com>
2016-04-14 17:03:41 +0900

    fix scale_x bug.

--- lib/smalruby/character.rb
+++ lib/smalruby/character.rb
@@ -207,17 +207,17 @@ module Smalruby
       @vector[:y] = Math.sin(radian)
 
       if @rotation_style == :free
-        self.scale_x = 1
+        self.scale_x = scale_x.abs
         super(val)
       elsif @rotation_style == :left_right
         if @vector[:x] >= 0
-          self.scale_x = 1
+          self.scale_x = scale_x.abs
         else
-          self.scale_x = -1
+          self.scale_x = scale_x.abs * -1
         end
         super(0)
       else
-        self.scale_x = 1
+        self.scale_x = scale_x.abs
         super(0)
       end
     end

edcc13d0f5f60fb2e0f393056b79838dd929bdab
bonochof <takata@Ryosuke-PC.localdomain>
2017-12-22 17:22:59 +0900

    Add distance_to_mouse_pointer method.

--- lib/smalruby/character.rb
+++ lib/smalruby/character.rb
@@ -679,5 +679,12 @@ module Smalruby
     def calc_volume
       (255 * @volume / 100.0).to_i
     end
+
+    def distance_to_mouse_pointer
+      mx = Input.mouse_pos_x
+      my = Input.mouse_pos_y
+      distance = Math.sqrt((mx-self.x)**2 + (my-self.y)**2)
+      return distance.round(6)
+    end
   end
 end
